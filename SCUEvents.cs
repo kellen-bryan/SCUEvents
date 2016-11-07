3617ca895a7d12f222d66322d78dfd2aad3a3280
kellen-bryan <kbryan@scu.edu>
2016-11-07 15:16:28 -0800

    Add files via upload

---
+++ SCUEvents.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using Xamarin.Forms;
+
+namespace SCUEvents
+{
+	public class App : Application
+	{
+		public App()
+		{
+
+			MainPage = new NavigationPage( new Mainpage());
+
+			/*// The root page of your application
+			var content = new ContentPage
+			{
+				Title = "SCUEvents",
+				Content = new StackLayout
+				{
+					VerticalOptions = LayoutOptions.Center,
+					Children = {
+						new Label {
+							HorizontalTextAlignment = TextAlignment.Center,
+							Text = "Welcome to Xamarin Forms!"
+						}
+					}
+				}*/
+			
+
+			//MainPage = new NavigationPage(content);
+		}
+
+		protected override void OnStart()
+		{
+			// Handle when your app starts
+		}
+
+		protected override void OnSleep()
+		{
+			// Handle when your app sleeps
+		}
+
+		protected override void OnResume()
+		{
+			// Handle when your app resumes
+		}
+	}
+}
